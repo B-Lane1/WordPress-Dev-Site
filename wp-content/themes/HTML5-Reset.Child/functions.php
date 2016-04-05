7eb946a0add9e9c9b37ad07115f89ab55d59059e
B-Lane1 <lanes782@hsd.k12.or.us>
2016-04-05 13:47:41 -0700

    added bootstrap to child theme

--- wp-content/themes/HTML5-Reset.Child/functions.php
+++ wp-content/themes/HTML5-Reset.Child/functions.php
@@ -8,6 +8,19 @@ function theme_enqueue_styles() {
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style )
     );
+	
+	wp_enqueue_style( 'bootstrap-min',
+        get_stylesheet_directory_uri() . '/css/bootstrap.min.css'
+    );
+	
+		wp_enqueue_style( 'bootstrap-theme-min',
+        get_stylesheet_directory_uri() . '/css/bootstrap-theme.min.css'
+    );
+	
+	wp_enqueue_script('bootstrap-min',
+		get_stylesheet_directory_uri() . '/js/bootstrap.min.js',
+		true
+	);
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 ?>
